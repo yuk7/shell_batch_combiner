c67a763aa824eb1cca52000bac28adeb724fbefa
yuk7 <yukx00@gmail.com>
2022-01-06 19:18:40 +0900

    combiner.sh: add warn with broken script

--- combiner.sh
+++ combiner.sh
@@ -16,13 +16,25 @@ LINUX_SOURCE="$2"
 
 DEST_FILE="$3"
 
+sh -n "$LINUX_SOURCE"
+if [ $? -ne 0 ]; then
+  echo [WARN] Input shell script has incorrect syntax.
+  echo
+fi
+
 
 CR=$(printf '\r')
 echo ":<<___FUNC_BATCH___ $CR" > "$DEST_FILE"
 cat "$WINDOWS_SOURCE" >> "$DEST_FILE"
 echo "$CR" >> "$DEST_FILE"
 echo "exit /b$CR" >> "$DEST_FILE"
-echo "" >> "$DEST_FILE"
 echo "___FUNC_BATCH___" >> "$DEST_FILE"
 echo >> "$DEST_FILE"
 cat "$LINUX_SOURCE" >> "$DEST_FILE"
+sh -n "$DEST_FILE"
+if [ $? -ne 0 ]; then
+  echo [WARN] Maybe Output script file is broken.
+  echo Output script has incorrect syntax.
+fi
+
+chmod +x "$DEST_FILE"
